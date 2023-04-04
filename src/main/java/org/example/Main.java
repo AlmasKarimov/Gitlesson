9e27d711e23eb1a865d92a0c135815d27880cb07
Каримов Алмас Ермекович <karimovae@velesstroy.com>
2023-04-04 20:12:03 +0500

    добавление в репозиторий

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        mainmethodShowmessageable();
+        mainmethodShowinfo();
+    }
+
+    private static void mainmethodShowmessageable(){
+            System.out.println ("hello");
+
+         }
+
+    private static void mainmethodShowinfo(){
+        System.out.println ("lets learn");
+
+    }
+
+
+}
