8a767b244d7e66d38e38f833860647b207026090
Каримов Алмас Ермекович <karimovae@velesstroy.com>
2023-04-05 20:51:48 +0500

    6 version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ public class Main {
          }
 
     private static void mainmethodShowinfo(){
-        System.out.println ("lets learn and read my book");
+        System.out.println ("lets learn and read my book, my magazine");
 
     }
 
