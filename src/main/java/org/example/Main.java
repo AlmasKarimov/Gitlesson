e06ec9a98413894f4434442714c59ef55e42692c
Каримов Алмас Ермекович <karimovae@velesstroy.com>
2023-04-05 20:26:48 +0500

    3 version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,17 @@ public class Main {
     public static void main(String[] args) {
         mainmethodShowmessageable();
         mainmethodShowinfo();
+        mainmethodShowmessageable1();
     }
 
     private static void mainmethodShowmessageable(){
             System.out.println ("hello almas. How are you");
 
          }
+    private static void mainmethodShowmessageable1(){
+        System.out.println ("I'm fine. and you55555555555???????");
 
+    }
     private static void mainmethodShowinfo(){
         System.out.println ("lets learn and read");
 
